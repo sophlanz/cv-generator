5f0aff66566c1c1880d5ce7f652b643b96923d00
Sophlanz <sophialanzendorf@gmail.com>
2022-10-21 08:34:54 -0400

    circle follows cursor on mousemove on homepage

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from './NavWelcome';
 import {Link} from 'react-router-dom';
 export default function Welcome () {
@@ -6,32 +6,41 @@ export default function Welcome () {
     const [y, setY] = useState(null);
     const [xChange,setXChange] = useState(null);
     const [yChange, setYChange] = useState(null);
-    const [style, setStyle] = useState(null);
+    /*circle styles */
+    const [left, setLeft] = useState(null);
+    const [right, setRight] = useState(null);
     const trackMouse = (e) => {
         //get x
         setX(e.clientX);
         //get y
         setY(e.clientY);
-        const xChange = e.clientX  * 0.008;
-        const yChange = e.clientY  * 0.08;
+        const xChange = e.clientX  * 0.01;
+        const yChange = e.clientY  * 0.01;
         setXChange(xChange);
         setYChange(yChange);
-       
     
     };
-    
+   
+    const setCursor = (e) => {
+        const x = e.pageX;
+            const y = e.pageY;
+            setLeft(`${x}px`)
+            console.log(left)
+            setRight(`${y}px`)
+            console.log(right)
+    };
         return (
-            <div className="gridContainer">
+            <div className="gridContainer"  onMouseMove={(e)=> setCursor(e)}>
                 <h1>CV Curator</h1>
                 {/*navbar */}
                 <Nav/>
                 {/*interactive ball, give translate3d style using state*/}
-                <div style={{transform:`translate3d(${xChange}px,${yChange}px, 0px) scale3d(1, 1, 1)`}} className="ball" onMouseMove={(e)=> trackMouse(e)} >
+                <div style={{transform:`translate3d(${xChange}%,${yChange}%, 1px)`}} className="ball" onMouseMove={(e)=> trackMouse(e)} >
                    <p><Link to={'/create-cv'}>Start Creating</Link></p> 
                 </div>
                 <p>{x},{y}</p>
+                {/*circle that will follow cursor */}
+                <div className="cursorCircle" style={{left:`${left}`, top:`${right}`}}></div>
             </div>
-            
         )
     }
-
