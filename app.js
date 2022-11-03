f6076b89117f5cc4a42353a23af6b27eada2afe1
Sophlanz <sophialanzendorf@gmail.com>
2022-11-02 22:12:00 -0400

    ran npm run build to create a build file for the client side

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 /* app.use(express.static(path.join(__dirname, 'public'))); */
 app.use(express.static(path.join(__dirname, "client", "build")))
+
 //connect db
 const dbConnect = require('./config/database')
 dbConnect();
